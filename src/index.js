265c97a853805f657b806042adfc4072ee0cdf88
Stoppa Matteo <stoppamatteo@gmail.com>
2025-10-26 19:07:52 +0100

    rimosso momentaneamente print variabili

--- src/index.js
+++ src/index.js
@@ -13,11 +13,11 @@ dotenv.config()
 
 const main = async () => {
     // info presenti nello script di avvio (visibili solo in windows con il "set" all'interno dello script di npm)
-    logger.info(`pnpm run script -> ENV: ${process.env.ENV}`)
-    logger.info(`pnpm run script -> ENV2: ${process.env.ENV2}`)
+    // logger.info(`pnpm run script -> ENV: ${process.env.ENV}`)
+    // logger.info(`pnpm run script -> ENV2: ${process.env.ENV2}`)
 
-    // Variabili comuni nella config del package.json
-    logger.info(`pnpm config -> VAR_A: ${process.env.npm_package_config_VAR_A}`);
+    // // Variabili comuni nella config del package.json
+    // logger.info(`pnpm config -> VAR_A: ${process.env.npm_package_config_VAR_A}`);
 
     // Variabili presenti nel file .env
     logger.info(`file env -> FILE_ENV_1: ${process.env.FILE_ENV_1}`)
